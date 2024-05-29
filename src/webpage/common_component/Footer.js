6911637ce214c2ea7ec75ee587276a4f7190bb65
huynhthao12a3 <huynhthao12a3@gmail.com>
2024-05-29 16:03:59 +0700

    HuynhThao test commit

--- src/webpage/common_component/Footer.js
+++ src/webpage/common_component/Footer.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import '../css/bootstrap.min.css'
-import '../css/style.css'
+import React, { Component } from "react";
+import "../css/bootstrap.min.css";
+import "../css/style.css";
 
 class Footer extends Component {
-    render() {
-        return (
-            <div className='footer-magin'>
-                <footer className="footer mt-auto py-3 bg-light">
-                    <div className="container">
-                        <span className="text-muted">
-                            © 2023 Your Company. All rights reserved. | Designed by You
-                        </span>
-                    </div>
-                </footer>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="footer-magin">
+				<footer className="footer mt-auto py-3 bg-light">
+					<div className="container">
+						<span className="text-muted">© 2023 Your Company. All rights reserved. | Designed by HuynhThao</span>
+					</div>
+				</footer>
+			</div>
+		);
+	}
 }
 
 export default Footer;
